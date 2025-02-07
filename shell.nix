bfa53def6142e394ceb390bc923c9ce64d765588
lillyliv7 <lillyliv1245@gmail.com>
2025-02-07 16:23:31 -0500

    improve project structure and build script

--- shell.nix
+++ shell.nix
@@ -8,14 +8,21 @@ pkgs.mkShellNoCC {
         pkgs.nodejs
         pkgs.typescript
         pkgs.python3
+        pkgs.yuicompressor
     ];
 
     shellHook = ''
         echo "Cleaning ./www/js"
         rm -rf ./www/js/*
-        echo "Compiling typescript files in ./typescript to ./www/js"
+        echo "Compiling Typescript files in ./typescript to ./www/js"
         tsc --project ./tsconfig.json
-        echo "Starting http server"
+        echo "Uglifying Javascript files with Yuicompressor"
+        find ./www/js -name "*.js" -size +0c -exec yuicompressor {} -o {} \;
+        echo "Uglifying CSS files with Yuicompressor"
+        find ./css -name "*.css" -exec sh -c 'yuicompressor "$0" -o "./www/css/$(basename "$0")"' {} \;
+        echo "Copying HTML files"
+        cp -r ./html/* ./www
+        echo "Starting HTTP server"
         cd www
         python3 -m http.server 8080
         echo "Server exited"
--- www/js/urlReformat.js
+++ www/js/urlReformat.js
@@ -1,4 +1,1 @@
-if (!window.location.href.endsWith(".html")) {
-    console.log("reformatting url to include index.html at the end");
-    window.location.href = window.location.href + "index.html";
-}
+if(!window.location.href.endsWith(".html")){console.log("reformatting url to include index.html at the end");window.location.href=window.location.href+"index.html"};
