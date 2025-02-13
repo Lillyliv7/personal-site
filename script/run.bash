846829e0e0945a750dd7dfb2006fbbc52159f352
lillyliv7 <lillyliv1245@gmail.com>
2025-02-13 15:21:24 -0500

    fix css files not being included

--- script/run.bash
+++ script/run.bash
@@ -11,7 +11,7 @@ echo "Uglifying Javascript files with Yuicompressor"
 # find ./www/js -name "*.js" -size +0c -exec yuicompressor -v {} -o {} \;
 echo "Uglifying CSS files with Yuicompressor"
 # cp -r css/. www/css/
-# find ./css -name "*.css" -exec sh -c 'yuicompressor -v "$0" -o "./www/css/$(basename "$0")"' {} \;
+find ./css -name "*.css" -exec sh -c 'yuicompressor -v "$0" -o "./www/css/$(basename "$0")"' {} \;
 echo "Uglifying HTML files"
 html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true --input-dir ./html --output-dir ./www
 cp -r ./res/* ./www/res
