f6dd6f43aad5c35bf58d71290f61edca13e54b5a
lillyliv7 <lillyliv1245@gmail.com>
2025-02-20 15:35:10 -0500

    remove ws stuff temporarily

--- typescript/main.ts
+++ typescript/main.ts
@@ -1,23 +1,3 @@
 function newTab(url: string){
     window.open(url, '_blank').focus();
 }
-
-
-const socket = new WebSocket("ws://imlillith888.xyz:3000", "stats-protocol");
-
-socket.onopen = () => {
-    console.log("Connected to WebSocket server with protocol:", socket.protocol);
-    socket.send("hello");
-};
-
-socket.onmessage = (event) => {
-    console.log("Received:", event.data);
-};
-
-socket.onerror = (error) => {
-    console.error("WebSocket error:", error);
-};
-
-socket.onclose = () => {
-    console.log("WebSocket connection closed");
-};
