e47c99afc717ab422cb9ee6fe8bc638d7bd5bc6d
lillyliv7 <lillyliv1245@gmail.com>
2025-02-21 21:11:19 -0500

    fix bug

--- script/serve.py
+++ script/serve.py
@@ -4,12 +4,15 @@ import time
 # Configure Flask to serve static files from the 'www' folder at the root URL.
 app = Flask(__name__, static_folder='../www', static_url_path='')
 
+
+@app.before_request
+def log_user():
+    f = open("../log/useragents.log","a")
+    f.write(request.remote_addr + ": " + request.headers.get('User-Agent')+ " at " + str(time.time())+ " for " + request.path + '\n')
+    f.close()
 # Explicitly serve index.html when accessing the root URL.
 @app.route('/')
 def index():
-    f = open("../log/useragents.log","a")
-    f.write(request.remote_addr + ": " + request.headers.get('User-Agent')+ " at " + str(time.time())+'\n')
-    f.close()
     return app.send_static_file('index.html')
 
 if __name__ == '__main__':
